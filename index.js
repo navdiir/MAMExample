9565197cc2c44fa728b082daee7bd8c8997597a7
@Chrislaive <christopher.alov@gmail.com>
2018-04-14 19:05:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const MAM = require('./lib/attachData.js')
+const IOTA = require('iota.lib.js')
+const iota = new IOTA()
+
+let timeLoop,date, 
+	i=1
+
+if( process.argv[2] == undefined){          //Getting the time in seconds for the loop
+  timeLoop = 60000                       //default 1 minute
+} else {
+  timeLoop = process.argv[2]*1000 
+}
+
+//Create a JSON as message
+
+function start(){
+	date = new Date().toISOString().replace('T', ' ').substr(0, 19)
+    let message = { 'Message' : i, 'Date' : date}
+	console.log('Start sending data to Tangle...')
+	let messageS = JSON.stringify(message)
+	console.log('Message: %s',messageS)
+	console.log('Message in trytes: ' + iota.utils.toTrytes(messageS))
+	MAM.attach(message)
+	console.log('--------------------------------------------------------------------------------------------------------------------')
+	i++
+}
+
+setInterval(function(){
+	start()
+},timeLoop)
